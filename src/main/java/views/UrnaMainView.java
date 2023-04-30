3d73dc0cccfad37f1f9dbbb70996506d8d21c169
Yuri Dias Pereira <ydpereira@dcomp.ufs.br>
2023-04-30 15:15:51 -0300

    Update View
    Implementado evento de clique do mouse para acionar "animação" de botões da urna.

--- src/main/java/views/UrnaMainView.java
+++ src/main/java/views/UrnaMainView.java
@@ -5,7 +5,6 @@ public class UrnaMainView {
     public static void main(String[] args) {
         if (!janelaAberta) {
             UrnaEletronicaView janela = new UrnaEletronicaView();
-            System.out.println("Caminho: " + System.getProperty("user.dir"));
             janela.setVisible(true);
             janelaAberta = true;
         }
