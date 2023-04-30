f4d84bd4da99297c12b380e8b88186fb42a0ff68
ytaloSA <ytalosantosargao@academico.ufs.br>
2023-04-30 18:38:24 -0300

    Só um erro de ortografia, ignora aí

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 import main.java.models.*;
-/**Para compilar ṕe necessário estar na pasta src e executar:
+/**Para compilar é necessário estar na pasta src e executar:
 javac main.Main.java
 Para executar, também é necessário estar na pasta src e usar o mesmo formato:
 java main.Main
