dc8bdf97c78c0b6bbada4eaacf15ade2581bd182
ytaloSA <ytalosantosargao@academico.ufs.br>
2023-05-01 20:08:49 -0300

    Podemos dizer que foi um progresso?

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,23 @@
 package main;
 import main.java.models.*;
+import main.java.controllers.*;
+import main.java.views.*;
+import java.util.ArrayList;
 /**Para compilar é necessário estar na pasta src e executar:
 javac main.Main.java
 Para executar, também é necessário estar na pasta src e usar o mesmo formato:
 java main.Main
-Pode ser main/Main.java ou main.Main que funciona igualmente**/
+Pode ser main/Main.java ou main.Main que funciona igualmente
+O trecho abaixo imprime separadamente cada um dos elementos
+Serve apenas para entender a sintaxe dos candidatos
+**/
 public class Main {
     public static void main(String[] args) {
-        
+        Candidatos objeto = new Candidatos();
+        ArrayList<Candidato> lista = ((ArrayList<Candidato>)objeto.obterCandidatosGenericos());
+        for (Candidato candidato:lista){
+            System.out.println(candidato.titulo);
+            System.out.println(candidato.dados);
     }
+}
 }
--- src/main/java/controllers/Candidatos.java
+++ src/main/java/controllers/Candidatos.java
@@ -7,26 +7,26 @@ import java.util.Set;
 import main.java.models.Candidato;
  public final class Candidatos {
     protected static Hashtable<String, List<String>> gerar(){
-        Hashtable<String,List<String>> candidatos;
-        candidatos = new Hashtable<String, List<String>>();
-        candidatos.put("Arroz por cima ou por baixo do feijão?",List.of("Por cima","Por baixo"));
-        candidatos.put("É Biscoito ou Bolacha?",List.of("Biscoito","Bolacha"));
-        candidatos.put("Qual o melhor?",List.of("Fifa","PES"));
-        candidatos.put("Qual a melhor  Editora de Quadrinhos?",List.of("Marvel","DC"));
-        candidatos.put("Xbox ou Playstaion?",List.of("Xbox","Playstation"));
-        candidatos.put("Melhor plataforma de jogos",List.of("PC","Console","Mobile"));
-        candidatos.put("Melhor Sistema Operacional de PC",List.of("Windows","Mac","Linux"));
-        candidatos.put("Melhor versão do Windows",List.of("7","10","11"));
-        candidatos.put("Carne mal passada é:",List.of("Crua","O melhor ponto"));
-        candidatos.put("É Beiju ou Tapioca?",List.of("Beiju","Tapioca"));
-        candidatos.put("Melhor Plataforma Mobile",List.of("Android","IOS"));
-        candidatos.put("Melhor marca de Smartphone Android",List.of("Samsung","Xiaomi"));
-        candidatos.put("Quem bota pra mamar?",List.of("AMD","Nvidia"));
-        candidatos.put("Nintendo",List.of("Compra","Pirateia sem dó"));
-        candidatos.put("Jogo Indie pode piratear?",List.of("Não. É imperdoável!","E qual é o problema?"));
-        return candidatos;
+        Hashtable<String,List<String>> genericos;
+        genericos = new Hashtable<String, List<String>>();
+        genericos.put("Arroz por cima ou por baixo do feijão?",List.of("Por cima","Por baixo"));
+        genericos.put("É Biscoito ou Bolacha?",List.of("Biscoito","Bolacha"));
+        genericos.put("Qual o melhor?",List.of("Fifa","PES"));
+        genericos.put("Qual a melhor  Editora de Quadrinhos?",List.of("Marvel","DC"));
+        genericos.put("Xbox ou Playstaion?",List.of("Xbox","Playstation"));
+        genericos.put("Melhor plataforma de jogos",List.of("PC","Console","Mobile"));
+        genericos.put("Melhor Sistema Operacional de PC",List.of("Windows","Mac","Linux"));
+        genericos.put("Melhor versão do Windows",List.of("7","10","11"));
+        genericos.put("Carne mal passada é:",List.of("Crua","O melhor ponto"));
+        genericos.put("É Beiju ou Tapioca?",List.of("Beiju","Tapioca"));
+        genericos.put("Melhor Plataforma Mobile",List.of("Android","IOS"));
+        genericos.put("Melhor marca de Smartphone Android",List.of("Samsung","Xiaomi"));
+        genericos.put("Quem bota pra mamar?",List.of("AMD","Nvidia"));
+        genericos.put("Nintendo",List.of("Compra","Pirateia sem dó"));
+        genericos.put("Jogo Indie pode piratear?",List.of("Não. É imperdoável!","E qual é o problema?"));
+        return genericos;
     }
-    protected static List<Candidato> candidatos(){
+    public List<Candidato> obterCandidatosGenericos(){
         List<Candidato> candidatos = new ArrayList<Candidato>();
         Hashtable<String,List<String>> dicionarioBase = gerar();
         Set<String> keys = dicionarioBase.keySet();
@@ -36,17 +36,4 @@ import main.java.models.Candidato;
         }
         return candidatos;
     }
-    /** Como usar cada elemento separadamente
-     * Use essa lógica para por o nome do candidato e suas opções na tela de votação
-     * O trecho abaixo imprime separadamente cada um dos elementos
-     * Serve apenas para entender a sintaxe
-     * O código ficou maior que o esperado pois resolvi embutir a lista generica de candidatos no próprio código
-    public static void main(String[] args) {
-        List<Candidato> lista = candidatos();
-        for (Candidato candidato:lista){
-            System.out.println(candidato.titulo);
-            System.out.println(candidato.dados);
-        }
-    }
-    **/
 }
