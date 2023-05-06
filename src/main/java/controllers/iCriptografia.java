4f1880ca790a52024961d93480645a0d416dda8c
ytaloSA <ytalosantosargao@academico.ufs.br>
2023-05-06 16:00:39 -0300

    HOOOOJE, a gente acaba esse projeto

---
+++ src/main/java/controllers/iCriptografia.java
@@ -0,0 +1,5 @@
+package main.java.controllers;
+public interface iCriptografia {
+    public static void criptografarArquivo(){}
+    public static void criptografarrquivoRAM(){}
+}
---
+++ src/main/java/controllers/iMain.java
@@ -0,0 +1,12 @@
+package main.java.controllers;
+public interface iMain{
+    public static void criarJanelaPrincipal(){}
+    public static void criarJanelaDeErro(){}
+    public static void iniciarVotacao(){}
+    public static void encerrarVotacao(){}
+    public static void cadastrarCandidato(){}
+    public static void cadastrarEleitor(){}
+    public static void verificacaoInicial(){}
+    public static void continuarVotacao(){}
+    public static void proximoEleitor(){}
+}
