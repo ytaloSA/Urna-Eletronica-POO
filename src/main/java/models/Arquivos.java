c0e7b7e12c700fb1619679c491098188f4cd1afc
ytaloSA <ytalosantosargao@academico.ufs.br>
2023-05-06 17:44:25 -0300

    modelo de arquivos txt criados e um deles pode ser processado, vou fazer o resto. Por se tratar de arquivos pequenos e ser mais simples de trabalhar usando Scanner, optei pelo Scanner pois ele facilita trabalhar com Strings

--- src/main/java/models/Arquivos.java
+++ src/main/java/models/Arquivos.java
@@ -1,18 +1,37 @@
 package main.java.models;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.awt.List;
+import java.util.Arrays;
 public class Arquivos {
-    public static void lerTxt(String arquivotxt){
+    public static ArrayList<String[]> lerTxt(String arquivotxt){
         final String raizUsusario = System.getProperty("user.home");
-        final String arquivo = raizUsusario + arquivotxt;
-        ArrayList<String> linhas = new ArrayList<String>();
+        final String arquivo;
+        if (arquivotxt == "candidatos.txt"){arquivo = "candidatos.txt";}
+        else{arquivo = raizUsusario + arquivotxt;}
+        ArrayList<String[]> linhas = new ArrayList<String[]>();
+        Scanner leitor;
         try {
-            FileReader file = new FileReader(arquivo);
-            BufferedReader leitor = new BufferedReader(file);
-        } catch (Exception e) {
-            // TODO: handle exception
+            leitor = new Scanner(new File(arquivo));
+            while(leitor.hasNext()){
+                String linha = leitor.next();
+                if(linha.contains("Candidato:")){
+                    String[] partes =  linha.split(",");
+                    String candidato = partes[0].replace("Candidato:", "");
+                    String id = partes[1].replace("Id:", "");
+                    String[] valores = {candidato, id};
+                    linhas.add(valores);
+                }
+            }
+            leitor.close();
+        } catch (FileNotFoundException e) {
+            return linhas;
         }
+        return linhas;
     }
 }
